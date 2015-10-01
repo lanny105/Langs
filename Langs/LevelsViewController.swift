d65bef5a8baa968819c7ce5c93ad71dc3cfd611b
Chuan Ren <renkelvin@gmail.com>
2015-09-30 21:16:03 -0700

    Squashed commit of the following:
    commit 869aa1aa14db56423e71ff58e883da4a267d0c13
    Author: CaptainCan <wyja9110@gmail.com>
    Date:   Tue Sep 29 22:53:20 2015 -0700
    
        Fixed Tap gesture.
    
    commit f19b7048fac85aa0fd11d0766ed32f1a66584655
    Author: CaptainCan <wyja9110@gmail.com>
    Date:   Mon Sep 28 21:04:04 2015 -0700
    
        Basic views on Levels and Start.
    
    commit 1e0a70e48d32e6f74f0ff5398a5303345d1d7e5f
    Author: CaptainCan <wyja9110@gmail.com>
    Date:   Mon Sep 28 16:31:03 2015 -0700
    
        Added LevelsView
    
    commit 8d178a7db9f7a06638fab2dc151846e6d7446795
    Author: CaptainCan <wyja9110@gmail.com>
    Date:   Mon Sep 28 15:39:02 2015 -0700
    
        ignore
    
    commit df31cbc8e945737fb1c2ac0955a76fc032180738
    Author: CaptainCan <wyja9110@gmail.com>
    Date:   Mon Sep 28 15:33:05 2015 -0700
    
        ignore
    
    commit 76c003785cb32315333aab22f0ec62d69b50c9ca
    Author: CaptainCan <wyja9110@gmail.com>
    Date:   Mon Sep 28 15:25:34 2015 -0700
    
        Added StartViewController and LevelsViewController.
    
    commit 0386d7cac09e32b1a2cef1f447da8cee847aa698
    Author: CaptainCan <wyja9110@gmail.com>
    Date:   Tue Sep 22 15:06:20 2015 -0700
    
        Updated README.md.

--- Langs/LevelsViewController.swift
+++ Langs/LevelsViewController.swift
@@ -6,4 +6,141 @@
 //  Copyright © 2015 mg526. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import QuartzCore
+import SceneKit
+
+class LevelsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // create a new scene
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
+        
+        // create and add a camera to the scene
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        scene.rootNode.addChildNode(cameraNode)
+        
+        // place the camera
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
+        
+        
+        
+        // create and add an ambient light to the scene
+        let ambientLightNode = SCNNode()
+        ambientLightNode.light = SCNLight()
+        ambientLightNode.light!.type = SCNLightTypeAmbient
+        ambientLightNode.light!.color = UIColor.darkGrayColor()
+        scene.rootNode.addChildNode(ambientLightNode)
+        
+//        // retrieve the ship node
+//        let ship = scene.rootNode.childNodeWithName("ship", recursively: true)!
+//        
+//        // animate the 3d object
+//        ship.runAction(SCNAction.repeatActionForever(SCNAction.rotateByX(0, y: 2, z: 0, duration: 1)))
+        
+        // add level info with pic
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named: "level1-1.png")
+        
+        // add level box
+        let boxGeometry = SCNBox(width: 4, height: 4, length: 4, chamferRadius: 0.4)
+        boxGeometry.materials = [material]
+        let boxNode = SCNNode(geometry: boxGeometry)
+        boxNode.position = SCNVector3(0, 0, 0)
+        
+        scene.rootNode.addChildNode(boxNode)
+        
+        // create and add a light to the scene
+        let lightNode = SCNNode()
+        lightNode.light = SCNLight()
+        lightNode.light!.type = SCNLightTypeOmni
+        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
+        scene.rootNode.addChildNode(lightNode)
+        
+        // retrieve the SCNView
+        let scnView = self.view as! SCNView
+        
+        // set the scene to the view
+        scnView.scene = scene
+        
+        // allows the user to manipulate the camera
+        scnView.allowsCameraControl = true
+        
+        // show statistics such as fps and timing information
+        scnView.showsStatistics = true
+        
+        // configure the view
+        scnView.backgroundColor = UIColor.blackColor()
+        
+        
+        
+        // add a tap gesture recognizer
+        let tapGesture = UITapGestureRecognizer(target: self, action: "handleTap:")
+        scnView.addGestureRecognizer(tapGesture)
+    }
+    
+    func handleTap(gestureRecognize: UIGestureRecognizer) {
+        // retrieve the SCNView
+        let scnView = self.view as! SCNView
+        
+        // check what nodes are tapped
+        let p = gestureRecognize.locationInView(scnView)
+        let hitResults = scnView.hitTest(p, options: nil)
+        // check that we clicked on at least one object
+        if hitResults.count > 0 {
+            // retrieved the first clicked object
+            let result: AnyObject! = hitResults[0]
+            
+            // get its material
+            let material = result.node!.geometry!.firstMaterial!
+            
+            // highlight it
+            SCNTransaction.begin()
+            SCNTransaction.setAnimationDuration(0.5)
+            
+            // on completion - unhighlight
+            SCNTransaction.setCompletionBlock {
+                SCNTransaction.begin()
+                SCNTransaction.setAnimationDuration(0.5)
+                
+                material.emission.contents = UIColor.blackColor()
+                
+                SCNTransaction.commit()
+            }
+            
+            material.emission.contents = UIColor.redColor()
+            
+            SCNTransaction.commit()
+            
+            self.performSegueWithIdentifier("levelsViewToGameViewSegue", sender: nil)
+        }
+        
+        
+    }
+    
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
+    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
+            return .AllButUpsideDown
+        } else {
+            return .All
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+}
