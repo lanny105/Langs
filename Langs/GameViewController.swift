8361965b993a82654d6d5b883e6a932e1933f370
Chuan Ren <renkelvin@gmail.com>
2015-09-30 21:13:56 -0700

    Squashed commit of the following:
    commit 5c1198e32ea875d2710e8a0d2d72e66d8375bd62
    Author: VeraWu <VW@Veras-MacBook-Pro.local>
    Date:   Wed Sep 30 10:12:36 2015 -0700
    
        update
    
    commit c9d3a908cf45d73bfd0ef6790b83dae83eda676f
    Author: CaptainCan <wyja9110@gmail.com>
    Date:   Tue Sep 29 22:47:37 2015 -0700
    
        Fixed camera control.
    
    commit 1335c8a4578d47c43596ea0365aa24a5b865b336
    Author: VeraWu <VW@Veras-MacBook-Pro.local>
    Date:   Tue Sep 29 21:23:25 2015 -0700
    
        addline
    
    commit 545a92ee980a6c9cb56b7fe1b7756affc7336f29
    Author: Boyangs-MacBook-Pro.local <bluedfreedom@gmail.com>
    Date:   Tue Sep 29 12:52:05 2015 -0700
    
        add makeTouchArea
    
    commit 0e2f761230907f7d7d2839706983038c3dd69693
    Author: VeraWu <VW@Veras-MacBook-Pro.local>
    Date:   Tue Sep 29 10:45:10 2015 -0700
    
        add highlight function
    
    commit df7855b67349d24fb1c03a7c90a819dc6d435fab
    Author: VeraWu <VW@Veras-MacBook-Pro.local>
    Date:   Mon Sep 28 16:36:34 2015 -0700
    
        gitignore
    
    commit 9ca90990e1a36c9380cf9f204222e5d543739086
    Author: Boyangs-MacBook-Pro.local <bluedfreedom@gmail.com>
    Date:   Mon Sep 28 16:32:29 2015 -0700
    
        gitignore
    
    commit 74ed662a040860be65b6374114772d1a2e750519
    Author: VeraWu <VW@Veras-MacBook-Pro.local>
    Date:   Mon Sep 28 15:32:30 2015 -0700
    
        ignore
    
    commit b13bfb8a89bc6f5e4a452eebbff787b7e6aa3ac0
    Author: VeraWu <VW@Veras-MacBook-Pro.local>
    Date:   Mon Sep 28 15:27:11 2015 -0700
    
        wrr
    
    commit 09b2883710667ca79b3ca5e0b6745efdc5384198
    Author: Boyangs-MacBook-Pro.local <bluedfreedom@gmail.com>
    Date:   Mon Sep 28 15:12:34 2015 -0700
    
        test

--- Langs/GameViewController.swift
+++ Langs/GameViewController.swift
@@ -1,9 +1,9 @@
 //
 //  GameViewController.swift
-//  Langs
+//  StarII
 //
-//  Created by Chuan Ren on 9/22/15.
-//  Copyright (c) 2015 mg526. All rights reserved.
+//  Created by Vera Wu on 9/26/15.
+//  Copyright (c) 2015 YunruWu. All rights reserved.
 //
 
 import UIKit
@@ -11,95 +11,247 @@ import QuartzCore
 import SceneKit
 
 class GameViewController: UIViewController {
-
-    override func viewDidLoad() { // test
+    
+    var sceneView: SCNView!
+    var camera: SCNNode!
+//    var ground: SCNNode!
+    var light: SCNNode!
+    
+    var sphere1: SCNNode!
+    var sphere2: SCNNode!
+    var sphere3: SCNNode!
+    var sphere4: SCNNode!
+    var sphere5: SCNNode!
+    
+    var line: SCNNode!
+    var starLines = [SCNNode]()
+    var starHighLights = [SCNNode]()
+    var highLightedStars = [SCNNode]()
+    var stars = [SCNNode]()
+    
+    var touchsphere: SCNNode!
+    var starTouchAreas = [SCNNode]()
+    
+    override func viewDidLoad() {
         super.viewDidLoad()
         
-        // create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        sceneView = SCNView(frame: self.view.frame)
+        sceneView.scene = SCNScene()
+        self.view.addSubview(sceneView)
         
-        // create and add a camera to the scene
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
-        scene.rootNode.addChildNode(cameraNode)
+//        let groundGeometry = SCNFloor()
+//        groundGeometry.reflectivity = 0
+//        let groundMaterial = SCNMaterial()
+//        groundMaterial.diffuse.contents = UIColor.blueColor()
+//        groundGeometry.materials = [groundMaterial]
+//        ground = SCNNode(geometry: groundGeometry)
+//        ground.position = SCNVector3(x: 0, y: -30, z: -20)
+//        ground.hidden = false
         
-        // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
+//                let earthGeometry = SCNSphere(radius: 150)
+//                let earthMaterial = SCNMaterial()
+//                earthMaterial.diffuse.contents = UIColor.blueColor()
+//                earthGeometry.materials = [earthMaterial]
+//                sphere3 = SCNNode(geometry: earthGeometry)
+//                sphere3.position = SCNVector3(x: 0, y: -20, z: 0)
         
-        // create and add a light to the scene
-        let lightNode = SCNNode()
-        lightNode.light = SCNLight()
-        lightNode.light!.type = SCNLightTypeOmni
-        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
-        scene.rootNode.addChildNode(lightNode)
+        let camera = SCNCamera()
+        camera.zFar = 10000
+        self.camera = SCNNode()
+        self.camera.camera = camera
+        self.camera.position = SCNVector3(x: -20, y: 15, z: 20)
+//        let constraint = SCNLookAtConstraint(target: ground)
+//        constraint.gimbalLockEnabled = true
+//        self.camera.constraints = [constraint]
         
-        // create and add an ambient light to the scene
-        let ambientLightNode = SCNNode()
-        ambientLightNode.light = SCNLight()
-        ambientLightNode.light!.type = SCNLightTypeAmbient
-        ambientLightNode.light!.color = UIColor.darkGrayColor()
-        scene.rootNode.addChildNode(ambientLightNode)
+        let ambientLight = SCNLight()
+        ambientLight.color = UIColor.darkGrayColor()
+        ambientLight.type = SCNLightTypeAmbient
+        self.camera.light = ambientLight
         
-        // retrieve the ship node
-        let ship = scene.rootNode.childNodeWithName("ship", recursively: true)!
+        let spotLight = SCNLight()
+        spotLight.type = SCNLightTypeSpot
+        spotLight.castsShadow = true
+        spotLight.spotInnerAngle = 70.0
+        spotLight.spotOuterAngle = 90.0
+        spotLight.zFar = 1000
+        light = SCNNode()
+        light.light = spotLight
+        light.position = SCNVector3(x: -20, y: 15, z: 25)
+//        light.constraints = [constraint]
         
-        // animate the 3d object
-        ship.runAction(SCNAction.repeatActionForever(SCNAction.rotateByX(0, y: 2, z: 0, duration: 1)))
+        let sphereGeometry = SCNSphere(radius: 0.3)
+        let sphereMaterial = SCNMaterial()
+        sphereMaterial.diffuse.contents = UIColor.whiteColor()
+        sphereGeometry.materials = [sphereMaterial]
+        sphere1 = SCNNode(geometry: sphereGeometry)
+        sphere1.position = SCNVector3(x: -20, y: 25, z: -3)
+        sphere2 = SCNNode(geometry: sphereGeometry)
+        sphere2.position = SCNVector3(x: -30, y: 20, z: 0)
+        sphere3 = SCNNode(geometry: sphereGeometry)
+        sphere3.position = SCNVector3(x: -10, y: 18, z: -1)
+        sphere4 = SCNNode(geometry: sphereGeometry)
+        sphere4.position = SCNVector3(x: -15, y: 10, z: -1)
+        sphere5 = SCNNode(geometry: sphereGeometry)
+        sphere5.position = SCNVector3(x: -27, y: 12, z: 2)
+        stars.append(sphere1)
+        stars.append(sphere2)
+        stars.append(sphere3)
+        stars.append(sphere4)
+        stars.append(sphere5)
         
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
-        // set the scene to the view
-        scnView.scene = scene
+        sceneView.scene?.rootNode.addChildNode(self.camera)
+//        sceneView.scene?.rootNode.addChildNode(ground)
+        sceneView.scene?.rootNode.addChildNode(light)
+        sceneView.scene?.rootNode.addChildNode(sphere1)
+        sceneView.scene?.rootNode.addChildNode(sphere2)
+        sceneView.scene?.rootNode.addChildNode(sphere3)
+        sceneView.scene?.rootNode.addChildNode(sphere4)
+        sceneView.scene?.rootNode.addChildNode(sphere5)
         
         // allows the user to manipulate the camera
-        scnView.allowsCameraControl = true
+        sceneView.allowsCameraControl = true
         
         // show statistics such as fps and timing information
-        scnView.showsStatistics = true
+        sceneView.showsStatistics = false
         
         // configure the view
-        scnView.backgroundColor = UIColor.blackColor()
+        sceneView.backgroundColor = UIColor.blackColor()
         
         // add a tap gesture recognizer
-        let tapGesture = UITapGestureRecognizer(target: self, action: "handleTap:")
-        scnView.addGestureRecognizer(tapGesture)
+        let tapRecognizer = UITapGestureRecognizer()
+        tapRecognizer.numberOfTapsRequired = 1
+        tapRecognizer.numberOfTouchesRequired = 1
+        tapRecognizer.addTarget(self, action: "sceneTapped:")
+        sceneView.addGestureRecognizer(tapRecognizer)
+//        sceneView.gestureRecognizers = [tapRecognizer]
+        
+        
+//        makeTouchArea()
+        makeHighLights()
+        
     }
     
-    func handleTap(gestureRecognize: UIGestureRecognizer) {
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
-        // check what nodes are tapped
-        let p = gestureRecognize.locationInView(scnView)
-        let hitResults = scnView.hitTest(p, options: nil)
-        // check that we clicked on at least one object
-        if hitResults.count > 0 {
-            // retrieved the first clicked object
-            let result: AnyObject! = hitResults[0]
+    func makeHighLights() {
+        for star in self.stars {
+            let sphereGeometry1 = SCNSphere(radius: 0.3)
+            let sphereMaterial1 = SCNMaterial()
+            sphereMaterial1.diffuse.contents = UIColor.yellowColor()
+            sphereGeometry1.materials = [sphereMaterial1]
             
-            // get its material
-            let material = result.node!.geometry!.firstMaterial!
+            let starHighLight = SCNNode(geometry: sphereGeometry1)
+            starHighLight.position = star.position
+            starHighLight.hidden = true
             
-            // highlight it
-            SCNTransaction.begin()
-            SCNTransaction.setAnimationDuration(0.5)
-            
-            // on completion - unhighlight
-            SCNTransaction.setCompletionBlock {
-                SCNTransaction.begin()
-                SCNTransaction.setAnimationDuration(0.5)
-                
-                material.emission.contents = UIColor.blackColor()
-                
-                SCNTransaction.commit()
-            }
-            
-            material.emission.contents = UIColor.redColor()
-            
-            SCNTransaction.commit()
+            sceneView.scene?.rootNode.addChildNode(starHighLight)
+            self.starHighLights.append(starHighLight)
         }
     }
+    
+//    func makeTouchArea(){
+//        for star in self.stars{
+//            let sphereGeometry1 = SCNSphere(radius: 2)
+//            let sphereMaterial1 = SCNMaterial()
+//            sphereMaterial1.diffuse.contents = UIColor.greenColor()
+//            sphereGeometry1.materials = [sphereMaterial1]
+//            let touchsphere = SCNNode(geometry: sphereGeometry1)
+//            //tochsphere.position = SCNVector3(x: star.position.x, y: star.position.y, z: star.position.z)
+//            touchsphere.position = star.position
+//            touchsphere.hidden = true
+//            star.addChildNode(touchsphere)
+//            self.starTouchAreas.append(touchsphere)
+//            
+//            //sceneView.backgroundColor=UIColor.blueColor()
+//        }
+//    }
+    
+    func sceneTapped(recognizer: UITapGestureRecognizer) {
+        let location = recognizer.locationInView(sceneView)
+        let hitResults = sceneView.hitTest(location, options: nil)
+        
+        if hitResults.count > 0 {
+            let result = hitResults[0] as! SCNHitTestResult
+            let node = result.node
+            var i = 0
+            if stars.contains(node) {
+                for star in self.stars {
+                    if node == star {
+                        //                      self.starTouchAreas[i].hidden = true //delete line will appear again!!!
+                        self.starHighLights[i].hidden = false
+                        self.highLightedStars.append(stars[i])
+                        if highLightedStars.count == 2 {
+                            line = lineBetweenNode(highLightedStars[0], nodeB: highLightedStars[1])
+                            sceneView.scene?.rootNode.addChildNode(line)
+                            self.starLines.append(line)
+                            highLightedStars.removeAll()
+                        }
+                    }
+                    
+                    i++
+                }
+                print(starLines)
+
+            }
+            else if starLines.contains(node) {
+                node.removeFromParentNode()
+            }
+            
+        }
+    }
+    
+//    func lineBetweenNode(nodeA: SCNNode, nodeB: SCNNode) -> SCNNode {
+////        let root = view.scene!.rootNode
+//        
+//        var cylinder: SCNNode!
+//        
+//        // visualize a sphere
+//        let sphere = SCNSphere(radius: 1)
+//        sphere.firstMaterial?.transparency = 0.5
+//        let sphereNode = SCNNode(geometry: sphere)
+//        sceneView.scene?.rootNode.addChildNode(sphereNode)
+//        
+//        // some dummy points opposite each other on the sphere
+//        let rootOneThird = Float(sqrt(1/3.0))
+//        let p1 = SCNVector3(x: rootOneThird, y: rootOneThird, z: rootOneThird)
+//        let p2 = SCNVector3(x: -rootOneThird, y: -rootOneThird, z: -rootOneThird)
+//        
+//        // height of the cylinder should be the distance between points
+//        let height = CGFloat(GLKVector3Distance(SCNVector3ToGLKVector3(p1), SCNVector3ToGLKVector3(p2)))
+//        
+//        // add a container node for the cylinder to make its height run along the z axis
+//        let zAlignNode = SCNNode()
+//        zAlignNode.eulerAngles.x = Float(M_PI_2)
+//        // and position the zylinder so that one end is at the local origin
+//        let cylinderGeometry = SCNCylinder(radius: 0.1, height: height)
+//        cylinder = SCNNode(geometry: cylinderGeometry)
+//        cylinder.position.y = Float(-height * 0.5)
+//        zAlignNode.addChildNode(cylinder)
+//        
+//        // put the container node in a positioning node at one of the points
+//        nodeB.addChildNode(zAlignNode)
+//        // and constrain the positioning node to face toward the other point
+//        nodeB.constraints = [ SCNLookAtConstraint(target: nodeA) ]
+////        return SCNNode(geometry: c)
+//    }
+    
+    
+    func lineBetweenNode(nodeA: SCNNode, nodeB: SCNNode) -> SCNNode {
+        let positions: [Float32] = [nodeA.position.x, nodeA.position.y, nodeA.position.z, nodeB.position.x, nodeB.position.y, nodeB.position.z]
+        let positionData = NSData(bytes: positions, length: sizeof(Float32)*positions.count)
+        let indices: [Int32] = [0, 1]
+        let indexData = NSData(bytes: indices, length: sizeof(Int32) * indices.count)
+        
+        let source = SCNGeometrySource(data: positionData, semantic: SCNGeometrySourceSemanticVertex, vectorCount: indices.count, floatComponents: true, componentsPerVector: 3, bytesPerComponent: sizeof(Float32), dataOffset: 0, dataStride: sizeof(Float32) * 3)
+        let element = SCNGeometryElement(data: indexData, primitiveType: SCNGeometryPrimitiveType.Line, primitiveCount: indices.count, bytesPerIndex: sizeof(Int32))
+        
+        let line = SCNGeometry(sources: [source], elements: [element])
+        return SCNNode(geometry: line)
+    }
+//
+//    func renderer(aRenderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: NSTimeInterval) {
+//        //Makes the lines thicker
+//        glLineWidth(100)
+//    }
     
     override func shouldAutorotate() -> Bool {
         return true
@@ -121,5 +273,5 @@ class GameViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-
+    
 }
