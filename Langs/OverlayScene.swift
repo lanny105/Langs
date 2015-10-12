c84668b2ac7b593f73c1f2f7d888ae6d976532ef
VeraWu <VW@Veras-MacBook-Pro.local>
2015-10-12 15:59:47 -0700

    add a pause button

--- Langs/OverlayScene.swift
+++ Langs/OverlayScene.swift
@@ -16,29 +16,38 @@ class OverlayScene: SKScene {
     let starXScale: CGFloat = 0.1
     let starYScale: CGFloat = 0.1
     let starImageNamed = "bdqx"
-    //let hintImageNamed = "hint"
     let hintImageNamed = "bdqx2"
     let finalImageNamed = "finish"
     
-    let
+    var pauseNode: SKSpriteNode!
+    
     
     override func didMoveToView(view: SKView) {
         
-        self.backgroundColor = UIColor.blackColor()
+        self.backgroundColor = UIColor.clearColor()
         
+        // add the pause button
+        let spriteSize = size.width/24
+        self.pauseNode = SKSpriteNode(imageNamed: "Pause Button")
+        self.pauseNode.size = CGSize(width: spriteSize, height: spriteSize)
+        self.pauseNode.position = CGPoint(x: spriteSize + 4, y: spriteSize + 4)
+        
+        self.addChild(self.pauseNode)
+        
+        // add the hint image
         makeHint()
     }
     
     func makeHint() {
-        let hintMap = SKSpriteNode(imageNamed:hintImageNamed)
-        hintMap.xScale = 0.3
+        let hintMap = SKSpriteNode(imageNamed: hintImageNamed)
+        hintMap.xScale = 0.25
         hintMap.yScale = 0.3
-        hintMap.position = CGPoint(x: 500, y: 60)
-        
+        hintMap.position = CGPoint(x: scene!.size.width * 0.85, y: scene!.size.height * 0.15)
         self.addChild(hintMap)
     }
     
     func makeHintFinal(){
+        let hintFinalSize = size.width/2
         let hintMap = SKSpriteNode(imageNamed:finalImageNamed)
         hintMap.xScale = 1.2
         hintMap.yScale = 1
